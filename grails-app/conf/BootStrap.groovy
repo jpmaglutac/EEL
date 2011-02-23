02188af751520cfd111a904272d70e7026c3cac6
Jesu Petar T. Maglutac <pepe@pepe-desktop.(none)>
2011-02-23 10:47:16 +0800

    Added roles to bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,10 @@
+import eel.*
+
 class BootStrap {
 
     def init = { servletContext ->
+    	(new Role(authority: "ROLE_STUDENT", description: "Student")).save(flush: true)
+    	(new Role(authority: "ROLE_TEACHER", description: "Teacher")).save(flush: true)
     }
     def destroy = {
     }
